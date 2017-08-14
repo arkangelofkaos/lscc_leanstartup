95634bf557a5732b49ba8155af3fdb9fa856d93e
Steve Haslam <steve.haslam@timgroup.com>
2017-08-14 17:32:27 +0100

    SteveH: rename LauncherTest since it doesn't actually test Launcher

--- src/test/java/com/timgroup/blankapp/AppTest.java
+++ src/test/java/com/timgroup/blankapp/AppTest.java
@@ -8,7 +8,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasItem;
 
-public class LauncherTest extends IntegrationTest {
+public class AppTest extends IntegrationTest {
     @Test public void
     status_page_contains_app_name() throws Exception {
         assertThat(read(server.uri("/info/status")), hasItem(containsString(AppName)));
